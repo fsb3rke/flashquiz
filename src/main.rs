1b98b2cdd5f9551a638321e6ce05efaed7b1a08f
0x704 <berkeeavcii@gmail.com>
2025-09-06 19:20:26 +0300

    feat: add CLI flashcard viewer and quiz mode

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod flashcard;
 mod quiz;
 
 use std::path::Path;
+use std::io::{self, Write};
 
 fn main() -> anyhow::Result<()> {
     let dir = Path::new("./flashcards");
@@ -14,15 +15,31 @@ fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    for file in files {
-        println!("\n==> File: {}", file);
-        let path = Path::new(&file);
-        let flashcards = fs_utils::read_flashcard_file(path)?;
-
-        flashcard::show_flashcards(&flashcards);
-
-        quiz::start_quiz(&flashcards);
+    println!("Existed flashcard files:");
+    for (i, file) in files.iter().enumerate() {
+        println!("\t[{}] {}", i+1, file);
     }
+
+    print!("Select flashcard file (index)");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let choice: usize = input.trim().parse().unwrap_or(0);
+
+    if choice == 0 || choice > files.len() {
+        println!("Invalid selection");
+        return Ok(());
+    }
+
+    let file = &files[choice-1];
+    println!("\n==> Selected File: {}", file);
+
+    let path = Path::new(file);
+    let flashcards = fs_utils::read_flashcard_file(path)?;
+
+    flashcard::show_flashcards(&flashcards);
+    quiz::start_quiz(&flashcards);
 
     Ok(())
 }
