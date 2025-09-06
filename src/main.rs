71b16a511a4ed2f270fe51ea026732dea1035682
0x704 <berkeeavcii@gmail.com>
2025-09-06 19:09:03 +0300

    feat: add code base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod fs_utils;
+mod models;
+mod flashcard;
+mod quiz;
+
+use std::path::Path;
+
+fn main() -> anyhow::Result<()> {
+    let dir = Path::new("./flashcards");
+    let files = fs_utils::list_json_files(dir)?;
+
+    if files.is_empty() {
+        println!("There's is no JSOn file in this directory");
+        return Ok(());
+    }
+
+    for file in files {
+        println!("\n==> File: {}", file);
+        let path = Path::new(&file);
+        let flashcards = fs_utils::read_flashcard_file(path)?;
+
+        flashcard::show_flashcards(&flashcards);
+
+        quiz::start_quiz(&flashcards);
+    }
+
+    Ok(())
 }
